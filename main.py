b5211f5b1c48bed3abd0ceac8e1ca4067c0fee80
jsearls-gh <jsearls90@gmail.com>
2024-11-28 06:58:25 -0500

    update main to cap framerate at 60fps

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("blue")
+        screen.fill("black")
         pygame.display.flip()
+        dt_ms = clock.tick(60)
+        dt = dt_ms / 1000
 
 if __name__ == "__main__":
     main()
